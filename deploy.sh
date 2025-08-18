8418c2fb3be429a7253023171cfd1b50c636a887
Rsantana <reinaldojsantana@gmail.com>
2025-08-17 21:42:57 -0300

    Ajuste finais de performance

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "🔨 Deploy projetos produto/consumidor ..."
 
-docker tag rinha-backend-2025-consumidor:v2 442494/rinha-backend-2025-consumidor:v4
-docker tag rinha-backend-2025-produtor:v2 442494/rinha-backend-2025-produtor:v4
+docker tag rinha-backend-2025-consumidor:v2 442494/rinha-backend-2025-consumidor:v6
+docker tag rinha-backend-2025-produtor:v2 442494/rinha-backend-2025-produtor:v6
 
-docker push 442494/rinha-backend-2025-produtor:v4
-docker push 442494/rinha-backend-2025-consumidor:v4
+docker push 442494/rinha-backend-2025-produtor:v6
+docker push 442494/rinha-backend-2025-consumidor:v6
 
 echo "🔨 Deploy concluído com sucesso!"
